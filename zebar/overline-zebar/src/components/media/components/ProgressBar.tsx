021ec45de5d7f79414f40a2c1e3f9aee65582295
Amit <amitbhagat860@gmail.com>
2025-06-27 19:45:03 +0530

    changed media icons ( play/pause )

--- zebar/overline-zebar/src/components/media/components/ProgressBar.tsx
+++ zebar/overline-zebar/src/components/media/components/ProgressBar.tsx
@@ -2,48 +2,48 @@ import { useState, useEffect } from "react";
 import { MediaSession } from "zebar";
 
 export function ProgressBar({
-	currentSession,
+    currentSession,
 }: {
-	currentSession: MediaSession | undefined;
+    currentSession: MediaSession | undefined;
 }) {
-	const [progress, setProgress] = useState<number>(
-		currentSession?.position ?? 0,
-	);
+    const [progress, setProgress] = useState<number>(
+        currentSession?.position ?? 0,
+    );
 
-	useEffect(() => {
-		if (!currentSession?.position) {
-			setProgress(0);
-			return;
-		}
+    useEffect(() => {
+        if (!currentSession?.position) {
+            setProgress(0);
+            return;
+        }
 
-		setProgress(currentSession.position);
+        setProgress(currentSession.position);
 
-		const interval = setInterval(() => {
-			setProgress((prev) => {
-				if (prev < currentSession.endTime) {
-					if (!currentSession.isPlaying) {
-						return prev;
-					}
-					return prev + 1;
-				} else {
-					setProgress(0);
-					clearInterval(interval);
-					return prev;
-				}
-			});
-		}, 1000);
+        const interval = setInterval(() => {
+            setProgress((prev) => {
+                if (prev < currentSession.endTime) {
+                    if (!currentSession.isPlaying) {
+                        return prev;
+                    }
+                    return prev + 1;
+                } else {
+                    setProgress(0);
+                    clearInterval(interval);
+                    return prev;
+                }
+            });
+        }, 1000);
 
-		return () => clearInterval(interval);
-	}, [currentSession, currentSession?.position]);
+        return () => clearInterval(interval);
+    }, [currentSession, currentSession?.position]);
 
-	return (
-		<div
-			className="absolute m-[2px] h-[82%] rounded-[0.5rem] bg-primary bottom-0 left-0 transition-[width] duration-200 ease-out -z-40"
-			style={{
-				width: currentSession
-					? `${(progress / currentSession?.endTime) * 100 || 0}%`
-					: "0",
-			}}
-		></div>
-	);
+    return (
+        <div
+            className="absolute m-[2px] h-[82%] rounded-[0.5rem] bg-primary bottom-0 left-0 transition-[width] duration-200 ease-out -z-40"
+            style={{
+                width: currentSession
+                    ? `${(progress / currentSession?.endTime) * 100 || 0}%`
+                    : "0",
+            }}
+        ></div>
+    );
 }
--- zebar/overline-zebar/src/components/media/components/Status.tsx
+++ zebar/overline-zebar/src/components/media/components/Status.tsx
@@ -1,62 +1,73 @@
 import { AnimatePresence, motion } from "framer-motion";
 import {
-	ArrowDownUp,
-	Pause,
-	Play,
-	SkipBack,
-	SkipForward,
-	CirclePause,
-	CirclePlay,
+    ArrowDownUp,
+    Pause,
+    Play,
+    SkipBack,
+    SkipForward,
+    CirclePause,
+    CirclePlay,
 } from "lucide-react";
 import { usePlayPause } from "../../../utils/usePlayPause";
+import { cn } from "../../../utils/cn";
+import "remixicon/fonts/remixicon.css";
 
 export enum PlayPauseState {
-	Played = "played",
-	Paused = "paused",
+    Played = "played",
+    Paused = "paused",
 }
 
 export enum StatusAction {
-	Previous = "previous",
-	Next = "next",
-	Switch = "switch",
+    Previous = "previous",
+    Next = "next",
+    Switch = "switch",
 }
 
-export function Status({ isPlaying }: { isPlaying: boolean }) {
-	const { isPlaying: state, statusAction: trackAction } =
-		usePlayPause(isPlaying);
+export function Status({ isPlaying }: { isPlaying: boolean; }) {
+    const { isPlaying: state, statusAction: trackAction } =
+        usePlayPause(isPlaying);
 
-	const icon = getIcon(trackAction, state);
-	const key = trackAction ?? (state ? "pause" : "play");
+    const icon = getIcon(trackAction, state);
+    const key = trackAction ?? (state ? "pause" : "play");
 
-	return (
-		<AnimatePresence mode="popLayout">
-			<motion.div
-				key={key}
-				initial={{ opacity: 0, scale: 0.5 }}
-				animate={{ opacity: 1, scale: 1.1 }} // 1.1 to mitigate the blurry icon
-				exit={{ opacity: 0, scale: 0.5 }}
-				transition={{ duration: 0.2 }}
-			>
-				<PlayPauseIcon LucideIcon={icon} />
-			</motion.div>
-		</AnimatePresence>
-	);
+    return (
+        <AnimatePresence mode="popLayout">
+            <motion.div
+                key={key}
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1.1 }} // 1.1 to mitigate the blurry icon
+                exit={{ opacity: 0, scale: 0.5 }}
+                transition={{ duration: 0.2 }}
+            >
+                {
+                    //<PlayPauseIcon LucideIcon={icon} />
+                }
+                <PlayPauseIcon_2 icon={icon} />
+            </motion.div>
+        </AnimatePresence>
+    );
 }
 
 function getIcon(trackAction: StatusAction | null, isPlaying: boolean) {
-	if (trackAction) {
-		if (trackAction === StatusAction.Previous) {
-			return SkipBack;
-		} else if (trackAction === StatusAction.Next) {
-			return SkipForward;
-		} else if (trackAction === StatusAction.Switch) {
-			return ArrowDownUp;
-		}
-	}
-	return isPlaying ? Pause : Play;
-	// return isPlaying ? CirclePause : CirclePlay;
+    if (trackAction) {
+        if (trackAction === StatusAction.Previous) {
+            return SkipBack;
+        } else if (trackAction === StatusAction.Next) {
+            return SkipForward;
+        } else if (trackAction === StatusAction.Switch) {
+            return ArrowDownUp;
+        }
+    }
+    // return isPlaying ? Pause : Play;
+    // return isPlaying ? CirclePause : CirclePlay;
+    return isPlaying ? "ri-play-circle-line" : "ri-pause-circle-line";
 }
 
-const PlayPauseIcon = ({ LucideIcon }: { LucideIcon: React.ElementType }) => (
-	<LucideIcon className="h-3 w-3" strokeWidth={3} />
+const PlayPauseIcon = ({ LucideIcon }: { LucideIcon: React.ElementType; }) => (
+    <LucideIcon className="h-3 w-3" strokeWidth={3} />
 );
+
+const PlayPauseIcon_2 = ({ icon }: { icon: string | any; }) => (
+    <i className={cn(icon, "strokeWidth={3} text-lg")} />
+
+)
